a45cec35e91a72cd862f296621f33c7afed6cf43
Stas Gavrylov <stasgavrylov@gmail.com>
2026-05-14 04:02:35 -0700

    Initial UI for Indexing tab

--- frontend/src/metabase-types/api/database.ts
+++ frontend/src/metabase-types/api/database.ts
@@ -57,7 +57,8 @@ export type DatabaseFeature =
   | "split-part"
   | "collate"
   | "transforms/python"
-  | "transforms/table";
+  | "transforms/table"
+  | "index/create-concurrently";
 
 export interface Database extends DatabaseData {
   id: DatabaseId;
